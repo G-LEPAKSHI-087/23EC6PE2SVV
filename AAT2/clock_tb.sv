d37656f8c48ced839cb7868ed5f7d5aea426d601
G-LEPAKSHI-087 <lepakshireddy640@gmail.com>
2026-03-03 15:28:27 +0530

    Update clock_tb.sv

--- AAT2/clock_tb.sv
+++ AAT2/clock_tb.sv
@@ -1,3 +1,14 @@
+//------------------------------------------------------------------------------
+// File        : clock_tb.sv
+// Author      : <G Lepakshi /1BM23EC087 >
+// Created     : <2026-01-4>
+// Module      : Top Module 
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+//
+// Description : Basic Digital Clock used for basic functional coverage example and uses a
+//               covergroup to measure input combination coverage.
+
 module clock_tb;
 
     logic clk = 0;
