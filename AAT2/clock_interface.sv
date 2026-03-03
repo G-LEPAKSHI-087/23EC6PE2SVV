1f5c326a1cf536fcb7b0b8d8221a43572519db0e
G-LEPAKSHI-087 <lepakshireddy640@gmail.com>
2026-03-03 15:29:36 +0530

    Update clock_interface.sv

--- AAT2/clock_interface.sv
+++ AAT2/clock_interface.sv
@@ -1,3 +1,14 @@
+//------------------------------------------------------------------------------
+// File        : clock_interface.sv
+// Author      : <G Lepakshi /1BM23EC087 >
+// Created     : <2026-03-1>
+// Module      : Interface
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+//
+// Description : Basic Digital Clock used for basic functional coverage example and uses a
+//               covergroup to measure input combination coverage.
+
 interface clock_interface (input logic clk);
 
    
