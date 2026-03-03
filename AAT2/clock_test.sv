483be69a3dc2c67c9f47d492b678a873ba01479b
G-LEPAKSHI-087 <lepakshireddy640@gmail.com>
2026-03-03 15:30:42 +0530

    Update clock_test.sv

--- AAT2/clock_test.sv
+++ AAT2/clock_test.sv
@@ -1,3 +1,14 @@
+//------------------------------------------------------------------------------
+// File        : clock_test.sv
+// Author      : <G Lepakshi /1BM23EC087 >
+// Created     : <2026-01-4>
+// Module      : Test Bench 
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+//
+// Description : Basic Digital Clock used for basic functional coverage example and uses a
+//               covergroup to measure input combination coverage.
+
 program clock_test(clock_interface.TB intf);
 
     // Functional Coverage
